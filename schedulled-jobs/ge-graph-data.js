eb2c5056c9bc6a5cb5c8755f73b25ebd3c98bcfc
fredrik62 <hogstrom10@gmail.com>
2018-07-27 01:49:26 +0300

    ..

--- schedulled-jobs/ge-graph-data.js
+++ schedulled-jobs/ge-graph-data.js
@@ -14,7 +14,7 @@ const GraphData = require('../models/item-graph');
 
 module.exports = {
 
-  getMeGraphData: function(req, res) {
+  getMeGraphData: function (req, res) {
     let linkId = [];
     Item.find({})
       .then((res) => {
@@ -26,64 +26,68 @@ module.exports = {
               .then(() => {
                 console.log("graphs deleted");
 
-              })
-          })
-        for (var key in res[0].data) {
-          if (res[0].data.hasOwnProperty(key)) {
-            //pushes all item ids to this array
-            let id = {
-              itemId: res[0].data[key].id
-            }
+                for (var key in res[0].data) {
+                  if (res[0].data.hasOwnProperty(key)) {
+                    //pushes all item ids to this array
+                    //these ids will be sent to a db collection then also used in this file
+                    let id = {
+                      itemId: res[0].data[key].id
+                    }
 
-            linkId.push(id.itemId);
-            var numberIds = linkId.filter(Number);
-          }
-        }
-
-        //item-ids.js model
-        var sendIdsToDB = ItemId({
-          id: numberIds
-        })
-        //saves item ids to the database
-        sendIdsToDB.save()
-          .then(item => {
-            console.log("saved to database");
-            //go into the item id collection and get all the item ids for the axios request
-            ItemId.find({})
-              .then((item) => {
-                const itemIdArray = item[0].id;
-                //length of idarray (arrlength)
-                const arrLength = itemIdArray.length;
-                for (var j = 0; j < arrLength; j++) {
-                  let urlArray = [base_URL + itemIdArray[j], graph_URL + itemIdArray[j] + '.json'] // unknown # of urls (1 or more)
-
-                  let promiseArray = urlArray.map(url => axios.get(url));
-                  axios.all(promiseArray)
-                    .then(function(results) {
-
-                      var temp = results.map(r => r.data.item);
-                      console.log(temp);
-                      var graph = GraphData({
-                        item: temp
-
-                      })
-
-                      graph.save()
-                        .then(item => {
-                          // console.log("graphdata saved to db!!!");
-                        })
-
-                        .catch((error) => {
-                          console.log(error);
-
-                        })
-                    })
-
+                    linkId.push(id.itemId);
+                    var numberIds = linkId.filter(Number);
+                  }
                 }
 
+                //item-ids.js model
+                //here the ids get sent to db
+                var sendIdsToDB = ItemId({
+                  id: numberIds
+                })
+                //saves item ids to the database
+                sendIdsToDB.save()
+                  .then(item => {
+                    console.log("saved to database");
+                    //go into the item id collection and get all the item ids for the axios request
+                    ItemId.find({})
+                      .then((item) => {
+                        const itemIdArray = item[0].id;
+                        //length of idarray (arrlength)
+                        const arrLength = itemIdArray.length;
+                        for (var j = 0; j < arrLength; j++) {
+                          let urlArray = [base_URL + itemIdArray[j], graph_URL + itemIdArray[j] + '.json'] // unknown # of urls (1 or more)
+
+                          let promiseArray = urlArray.map(url => axios.get(url));
+                          axios.all(promiseArray)
+                            .then(function (results) {
+
+                              var temp = results.map(r => r.data.item);
+                              console.log(temp);
+                              var graph = GraphData({
+                                item: temp
+
+                              })
+
+                              graph.save()
+                                .then(item => {
+                                  console.log("graphdata saved to db!!!");
+                                })
+
+                                .catch((error) => {
+                                  console.log(error);
+
+                                })
+                            })
+
+
+
+                        }
+
+                      })
+                  })
+
               })
           })
-
       })
   }
 }
